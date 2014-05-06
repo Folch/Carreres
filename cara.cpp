622c52f07f0f48bb22af0bc1dcf3d546f0527d07
ALBERT FOLCH GARCIA <afolchga8.alumnes@ub052028>
2014-05-06 18:41:40 +0200

    Updated

--- cara.cpp
+++ cara.cpp
@@ -20,28 +20,31 @@ Cara::Cara(int i1, int i2, int i3, int i4)
 }
 
 
-void Cara::calculaNormal(vector<Vertices> &v)
-{
-  int j;
-  normal.x=0.0;
-  normal.y=0.0;
-  normal.z=0.0;
-  int n = idxVertices.size();
-  for (int i=0; i<n; i++)
-  {
-    j = (i+1)%n;
+vec4 Cara::calculaNormal(vector<Vertices> &v) {
+    ////////////////////////////////////////////////////////////////////////////////////
+    ///////[0,1,2] = [B,C,A]
+    //2
+    vec4 vectorACx_ax =  v[idxVertices[2]].x - v[idxVertices[1]].x;
+    vec4 vectorACy_ay =  v[idxVertices[2]].y - v[idxVertices[1]].y;
+    vec4 vectorACz_az =  v[idxVertices[2]].z - v[idxVertices[1]].z;
 
-    normal.x += ((v[idxVertices[i]].z+v[idxVertices[j]].z)*
-                 (v[idxVertices[i]].y-v[idxVertices[j]].y));
-    normal.y += ((v[idxVertices[i]].x+v[idxVertices[j]].x)*
-                 (v[idxVertices[i]].z-v[idxVertices[j]].z));
-    normal.z += ((v[idxVertices[i]].y+v[idxVertices[j]].y)*
-                 (v[idxVertices[i]].x-v[idxVertices[j]].x));
-  }
+    //0
+    vec4 vectorBCx_bx =  v[idxVertices[0]].x - v[idxVertices[1]].x;
+    vec4 vectorBCy_by =  v[idxVertices[0]].y - v[idxVertices[1]].y;
+    vec4 vectorBCz_bz =  v[idxVertices[0]].z - v[idxVertices[1]].z;
 
-  normal.x *= 0.5;
-  normal.y *= 0.5;
-  normal.z *= 0.5;
-//  normal.normalize();
+
+
+    //|(a,b,c)|  = sqrt(a²+b²+c²)
+    normal.x = vectorACy_ay*vectorBCz_bz - vectorACz_az * vectorBCy_by;//A x B
+    normal.y = vectorACz_az*vectorBCx_bx - vectorACx_ax * vectorBCz_bz;
+    normal.z = vectorACx_ax*vectorBCy_by - vectorACy_ay*vectorBCx_bx;
+    GLfloat mod = sqrt(normal.x * normal.x +
+                       normal.y * normal.y +
+                       normal.z * normal.z);
+    normal.x /= mod;
+    normal.y /= mod;
+    normal.z /= mod;
+
 }
 
